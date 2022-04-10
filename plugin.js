a7632cf36ff3a2edeac08b01ff1fa399eeb32f06
Yahel PS <56220003+YahelPS@users.noreply.github.com>
2022-04-10 14:37:42 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,30 +1,12 @@
-function s(t) {
-  window.enmity.plugins.registerPlugin(t);
-}
-function a(t, e = !0) {
-  return window.enmity.getModule(t, e);
-}
-function u(t) {
-  return window.enmity.patcher.create(t);
-}
-const n = a((t) => {
-    var e;
-    return (e = t.default) == null
-      ? void 0
-      : e.displayName === "MessageContextMenu";
-  }),
-  g = {
-    name: "SilentTyping",
-    patches: [],
-    onStart() {
-      const t = u("silent-typing");
-      t.after(n.default, "startTyping", (e, i, r) => {
-        alert(JSON.stringify(i));
-      }),
-        this.patches.push(t);
-    },
-    onStop() {
-      this.patches = [];
-    },
-  };
-s(g);
+const { plugins, getModule, patcher } = window.enmity;
+
+plugins.registerPlugin({
+  name: "Greeter",
+  patches: [],
+  onStart() {
+    alert("Hello World!");
+  },
+  onStop() {
+    alert("Goodbye World!");
+  },
+});
